1d85e017598d3ea5ff9fe2db4847a7121e042638
Eva Jin <evayihuajin@gmail.com>
2026-04-14 15:04:35 -0400

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private: 
+    void quicksort(vector<int>& nums, int start, int end) {
+        if (start >= end) return;
+        int pivot = nums[end];
+        int i = start; 
+        for (int j = start; j < end; j++) {
+            if (nums[j] < pivot) {
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
+        swap(nums[i], nums[end]);
+        quicksort(nums, start, i-1);
+        quicksort(nums, i+1, end);
+    }
+
+    vector<vector<int>> result; 
+    void backtrack(vector<int>& nums, int start, vector<int>& current, int target, int sum) {
+        if (sum == target) {
+            result.push_back(current);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); i++) {
+            if (sum + nums[i] > target) break;
+            current.push_back(nums[i]);
+            backtrack(nums, i, current, target, sum + nums[i]);
+            current.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        if (nums.empty()) return {};
+
+        quicksort(nums, 0, nums.size()-1);
+        
+        vector<int> current = {};
+        backtrack(nums, 0, current, target, 0);
+
+        return result;
+    }
+};
