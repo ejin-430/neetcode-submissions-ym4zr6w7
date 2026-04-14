6f8983fff6b4bdd1e77c620357863bd48f72c4cb
Eva Jin <evayihuajin@gmail.com>
2026-04-14 12:35:18 -0400

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,31 @@
+class MedianFinder {
+private: 
+    priority_queue<int, vector<int>, greater<int>> minH; // for the larger half
+    priority_queue<int> maxH; // for the smaller half
+public:
+    MedianFinder() {
+        minH = {}; 
+        maxH = {}; 
+    }
+    
+    void addNum(int num) {
+        if (minH.empty() || num <= minH.top()) maxH.push(num); 
+        else minH.push(num); 
+
+        if (minH.size() > maxH.size() + 1) {
+            maxH.push(minH.top()); 
+            minH.pop();
+        }
+
+        if (maxH.size() > minH.size() + 1) {
+            minH.push(maxH.top()); 
+            maxH.pop();
+        }
+    }
+    
+    double findMedian() {
+        if (minH.size() == maxH.size()) return (minH.top() + maxH.top())/2.0;
+        else if (minH.size() > maxH.size()) return minH.top(); 
+        else return maxH.top();
+    }
+};
