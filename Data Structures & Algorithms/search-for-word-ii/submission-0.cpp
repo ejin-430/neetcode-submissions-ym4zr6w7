b249f778b4a17f4ba14f3a5072a072e76abbfb66
Eva Jin <evayihuajin@gmail.com>
2026-04-20 11:44:24 -0400

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    bool backtrack(vector<vector<char>>& board, string& word, int row, int col, int idx) {
+        if (idx == word.length()) return true;
+        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size()) return false;
+
+        // check current cell 
+        if (board[row][col] != word[idx]) return false;
+        
+        // mark current cell as visited
+        char c = board[row][col];
+        board[row][col] = '1';
+
+        // check all 4 directions
+        if (backtrack(board, word, row-1, col, idx+1) ||
+            backtrack(board, word, row+1, col, idx+1) ||
+            backtrack(board, word, row, col-1, idx+1) ||
+            backtrack(board, word, row, col+1, idx+1)) {
+            board[row][col] = c;
+            return true;
+        }
+
+        // update current cell
+        board[row][col] = c;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        if (word.empty()) return true;
+        if (board.empty()) return false;
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[i].size(); j++) {
+                if (backtrack(board, word, i, j, 0)) return true;
+            }
+        }
+
+        return false; 
+    }
+
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        vector<string> result = {}; 
+        for (int i = 0; i < words.size(); i++) {
+            if (exist(board, words[i])) result.push_back(words[i]);
+        }
+        return result;
+    }
+};
