7e1fa52082119c0ec32c0006f00506ce76a29cca
Eva Jin <evayihuajin@gmail.com>
2026-05-23 23:03:22 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+private: 
+    vector<int> s; 
+    vector<int> ms;
+public:
+    MinStack() {
+        s = {}; 
+    }
+    
+    void push(int val) {
+        s.push_back(val);
+        if (ms.empty()) ms.push_back(val);
+        else ms.push_back(min(val, ms.back()));
+    }
+    
+    void pop() {
+        s.erase(s.end());
+        ms.erase(ms.end());
+    }
+    
+    int top() {
+        return s.back(); 
+    }
+    
+    int getMin() {
+        return ms.back(); 
+    }
+};
